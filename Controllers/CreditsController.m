5edd477fb43bb8b28ca8c3f9ea0d394240959d8a
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-04-10 22:10:54 -0400

    Credits redesign

--- Controllers/CreditsController.m
+++ Controllers/CreditsController.m
@@ -57,70 +57,86 @@
             if(indexPath.row == 0) {
                 cell.textLabel.text = @"Lillie";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Developer";
             }
         }
         if(indexPath.section == 1) {
             if(indexPath.row == 0) {
                 cell.textLabel.text = @"Alpha_Stream";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Icon Designer";
             }
         }
         if(indexPath.section == 2) {
             if(indexPath.row == 0) {
                 cell.textLabel.text = @"Binny";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Alpha & Beta Tester";
             }
             if(indexPath.row == 1) {
                 cell.textLabel.text = @"Cameren";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Beta Tester";
             }
             if(indexPath.row == 2) {
                 cell.textLabel.text = @"Capt Inc";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Beta Tester";
             }
             if(indexPath.row == 3) {
                 cell.textLabel.text = @"Emma";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Alpha & Beta Tester";
             }
             if(indexPath.row == 4) {
                 cell.textLabel.text = @"Emy";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Beta Tester";
             }
             if(indexPath.row == 5) {
                 cell.textLabel.text = @"hmuy";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Beta Tester";
             }
             if(indexPath.row == 6) {
                 cell.textLabel.text = @"Lans";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Beta Tester";
             }
             if(indexPath.row == 7) {
                 cell.textLabel.text = @"PoomSmart";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Features: \"Adblock\", \"AutoPlay In Fullscreen\"";
             }
             if(indexPath.row == 8) {
                 cell.textLabel.text = @"Rick";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Beta Tester";
             }
             if(indexPath.row == 9) {
                 cell.textLabel.text = @"Rosie";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Beta Tester";
             }
             if(indexPath.row == 10) {
                 cell.textLabel.text = @"Sarah";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Alpha & Beta Tester";
             }
             if(indexPath.row == 11) {
                 cell.textLabel.text = @"Sloopie";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Beta Tester";
             }
             if(indexPath.row == 12) {
                 cell.textLabel.text = @"Worf";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Beta Tester";
             }
             if(indexPath.row == 13) {
                 cell.textLabel.text = @"Zachary";
                 cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                cell.detailTextLabel.text = @"Beta Tester, Features: \"Enable Extra Video Speed\"";
             }
         }
     }
@@ -185,29 +201,8 @@
     }
 }
 
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    if (section == 0) {
-        return @"Developers";
-    }
-    if (section == 1) {
-        return @"Icon Designer";
-    }
-    if (section == 2) {
-        return @"Special Thanks";
-    }
-    return nil;
-}
-
-- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section {
-    if (self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
-        view.tintColor = [UIColor colorWithRed:0.949 green:0.949 blue:0.969 alpha:1.0];
-    }
-    else {
-        view.tintColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
-    }
-    UITableViewHeaderFooterView *header = (UITableViewHeaderFooterView *)view;
-    [header.textLabel setTextColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"tableSection"]]];
-    [header.textLabel setFont:[UIFont systemFontOfSize:14]];
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    return [UIView new];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
