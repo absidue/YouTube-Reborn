1fc369b67ca720b5ee389bc694857c6da7d766c1
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-05-30 21:20:08 -0400

    Fix changelogs url

--- Controllers/ChangelogsController.m
+++ Controllers/ChangelogsController.m
@@ -11,7 +11,7 @@
 	[super loadView];
     
     _rebornChangelogsWebView = [[WKWebView alloc] initWithFrame:self.view.frame];  
-    [_rebornChangelogsWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://repo.lillieh.gay/changelogs/youtubereborn.php"]]];
+    [_rebornChangelogsWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://repo.lillieh.gay/changelogs/youtubereborn.html"]]];
     _rebornChangelogsWebView.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height);
     [self.view addSubview:_rebornChangelogsWebView];
 }
