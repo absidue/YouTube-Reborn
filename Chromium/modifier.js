f764999fb72a43c5dddb7b4a61dbbf3b5ef4cb58
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-06-17 22:57:30 -0400

    Add a lot more options, fix yt page changes

--- Chromium/modifier.js
+++ Chromium/modifier.js
@@ -1,32 +1,81 @@
-chrome.storage.sync.get({
-    enablePictureInPictureButtonOption: true,
-    hideVoiceSearchButtonOption: false,
-    hideAutoPlaySwitchOption: false,
-    hideCaptionsButtonOption: false,
-    hideInfoCardButtonOption: false,
-    hideMiniplayerButtonOption: false,
-    hideTheaterModeButtonOption: false
-}, function(items) {
-    if (items.enablePictureInPictureButtonOption == true) {
-        $(".ytp-pip-button").removeAttr("style");
-    }
-    if (items.hideVoiceSearchButtonOption == true) {
-        $("#voice-search-button").remove();
-    }
-    if (items.hideAutoPlaySwitchOption == true) {
-        $(".ytp-autonav-toggle-button-container").remove();
-        $("[data-tooltip-target-id='ytp-autonav-toggle-button']").remove();
-    }
-    if (items.hideCaptionsButtonOption == true) {
-        $(".ytp-subtitles-button").remove();
-    }
-    if (items.hideInfoCardButtonOption == true) {
-        $(".ytp-cards-button-icon").remove();
-    }
-    if (items.hideMiniplayerButtonOption == true) {
-        $(".ytp-miniplayer-button").remove();
-    }
-    if (items.hideTheaterModeButtonOption == true) {
-        $(".ytp-size-button").remove();
-    }
-});
+function run() { 
+    chrome.storage.sync.get({
+        // Video Options
+        enablePictureInPictureButtonOption: true,
+        disableAutoPlayOption: false,
+        disableCaptionsOption: false,
+        // NavBar Options
+        hideVoiceSearchButtonOption: false,
+        hideCountrySymbolNextToLogoOption: false,
+        // Overlay Options
+        hideAutoPlaySwitchOption: false,
+        hideCaptionsButtonOption: false,
+        hideInfoCardButtonOption: false,
+        hideMiniplayerButtonOption: false,
+        hideTheaterModeButtonOption: false,
+        hidePreviousButtonOption: false,
+        hideNextButtonOption: false
+    }, function(items) {
+        // Video Options
+
+        // Enable Picture In Picture Button
+        if (items.enablePictureInPictureButtonOption == true) {
+            $(".ytp-pip-button").removeAttr("style");
+        }
+        // Disable AutoPlay
+        if (items.disableAutoPlayOption == true) {
+            if ($(".ytp-autonav-toggle-button").attr("aria-checked") === "true") {
+                $(".ytp-button[data-tooltip-target-id='ytp-autonav-toggle-button']").click();
+            }
+        }
+        // Disable Captions
+        if (items.disableCaptionsOption == true) {
+            $(".ytp-subtitles-button[aria-pressed='true']").click();
+        }
+
+        // NavBar Options
+
+        // Hide Voice Search Button
+        if (items.hideVoiceSearchButtonOption == true) {
+            $("#voice-search-button").remove();
+        }
+        // Hide Country Symbol Next To Logo
+        if (items.hideCountrySymbolNextToLogoOption == true) {
+            $("#country-code").remove();
+        }
+
+        // Overlay Options
+
+        // Hide AutoPlay Switch
+        if (items.hideAutoPlaySwitchOption == true) {
+            $(".ytp-button[data-tooltip-target-id='ytp-autonav-toggle-button']").remove();
+        }
+        // Hide Captions Button
+        if (items.hideCaptionsButtonOption == true) {
+            $(".ytp-subtitles-button").remove();
+        }
+        // Hide InfoCard Button
+        if (items.hideInfoCardButtonOption == true) {
+            $(".ytp-cards-button-icon").remove();
+        }
+        // Hide Miniplayer Button
+        if (items.hideMiniplayerButtonOption == true) {
+            $(".ytp-miniplayer-button").remove();
+        }
+        // Hide Theather Mode Button
+        if (items.hideTheaterModeButtonOption == true) {
+            $(".ytp-size-button").remove();
+        }
+        // Hide Previous Button
+        if (items.hidePreviousButtonOption == true) {
+            $(".ytp-prev-button").remove();
+        }
+        // Hide Next Button
+        if (items.hideNextButtonOption == true) {
+            $(".ytp-next-button").remove();
+        }
+    });
+}
+
+window.onload = run;
+window.addEventListener('yt-navigate-start', run, true);
--- Chromium/options.js
+++ Chromium/options.js
@@ -1,22 +1,35 @@
 function save_options() {
     // Video Options
     var enablePictureInPictureButtonOption = document.getElementById('enablePictureInPictureButtonCheckBox').checked;
+    var disableAutoPlayOption = document.getElementById('disableAutoPlayCheckBox').checked;
+    var disableCaptionsOption = document.getElementById('disableCaptionsCheckBox').checked;
     // NavBar Options
     var hideVoiceSearchButtonOption = document.getElementById('hideVoiceSearchButtonCheckBox').checked;
+    var hideCountrySymbolNextToLogoOption = document.getElementById('hideCountrySymbolNextToLogoCheckBox').checked;
     // Overlay Options
     var hideAutoPlaySwitchOption = document.getElementById('hideAutoPlaySwitchCheckBox').checked;
     var hideCaptionsButtonOption = document.getElementById('hideCaptionsButtonCheckBox').checked;
     var hideInfoCardButtonOption = document.getElementById('hideInfoCardButtonCheckBox').checked;
     var hideMiniplayerButtonOption = document.getElementById('hideMiniplayerButtonCheckBox').checked;
     var hideTheaterModeButtonOption = document.getElementById('hideTheaterModeButtonCheckBox').checked;
+    var hidePreviousButtonOption = document.getElementById('hidePreviousButtonCheckBox').checked;
+    var hideNextButtonOption = document.getElementById('hideNextButtonCheckBox').checked;
     chrome.storage.sync.set({
+        // Video Options
         enablePictureInPictureButtonOption: enablePictureInPictureButtonOption,
+        disableAutoPlayOption: disableAutoPlayOption,
+        disableCaptionsOption: disableCaptionsOption,
+        // NavBar Options
         hideVoiceSearchButtonOption: hideVoiceSearchButtonOption,
+        hideCountrySymbolNextToLogoOption: hideCountrySymbolNextToLogoOption,
+        // Overlay Options
         hideAutoPlaySwitchOption: hideAutoPlaySwitchOption,
         hideCaptionsButtonOption: hideCaptionsButtonOption,
         hideInfoCardButtonOption: hideInfoCardButtonOption,
         hideMiniplayerButtonOption: hideMiniplayerButtonOption,
-        hideTheaterModeButtonOption: hideTheaterModeButtonOption
+        hideTheaterModeButtonOption: hideTheaterModeButtonOption,
+        hidePreviousButtonOption: hidePreviousButtonOption,
+        hideNextButtonOption: hideNextButtonOption
     }, function() {
         var status = document.getElementById('status');
         status.textContent = 'Options Saved';
@@ -28,21 +41,37 @@ function save_options() {
 
 function restore_options() {
     chrome.storage.sync.get({
+        // Video Options
         enablePictureInPictureButtonOption: true,
+        disableAutoPlayOption: false,
+        disableCaptionsOption: false,
+        // NavBar Options
         hideVoiceSearchButtonOption: false,
+        hideCountrySymbolNextToLogoOption: false,
+        // Overlay Options
         hideAutoPlaySwitchOption: false,
         hideCaptionsButtonOption: false,
         hideInfoCardButtonOption: false,
         hideMiniplayerButtonOption: false,
-        hideTheaterModeButtonOption: false
+        hideTheaterModeButtonOption: false,
+        hidePreviousButtonOption: false,
+        hideNextButtonOption: false
     }, function(items) {
+        // Video Options
         document.getElementById('enablePictureInPictureButtonCheckBox').checked = items.enablePictureInPictureButtonOption;
+        document.getElementById('disableAutoPlayCheckBox').checked = items.disableAutoPlayOption;
+        document.getElementById('disableCaptionsCheckBox').checked = items.disableCaptionsOption;
+        // NavBar Options
         document.getElementById('hideVoiceSearchButtonCheckBox').checked = items.hideVoiceSearchButtonOption;
+        document.getElementById('hideCountrySymbolNextToLogoCheckBox').checked = items.hideCountrySymbolNextToLogoOption;
+        // Overlay Options
         document.getElementById('hideAutoPlaySwitchCheckBox').checked = items.hideAutoPlaySwitchOption;
         document.getElementById('hideCaptionsButtonCheckBox').checked = items.hideCaptionsButtonOption;
         document.getElementById('hideInfoCardButtonCheckBox').checked = items.hideInfoCardButtonOption;
         document.getElementById('hideMiniplayerButtonCheckBox').checked = items.hideMiniplayerButtonOption;
         document.getElementById('hideTheaterModeButtonCheckBox').checked = items.hideTheaterModeButtonOption;
+        document.getElementById('hidePreviousButtonCheckBox').checked = items.hidePreviousButtonOption;
+        document.getElementById('hideNextButtonCheckBox').checked = items.hideNextButtonOption;
     });
 }
   
